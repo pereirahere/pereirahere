7099c938c98a1acdec4b7c91a30bcdb33aecc0db
João Pereira <joao.pereira@feedzai.com>
2021-12-09 11:58:33 +0000

    TEMPOLOG: Figuring out python requests

--- scripts/tempolog/tempolog.py
+++ scripts/tempolog/tempolog.py
@@ -1,9 +1,20 @@
-import os
+import requests
 import getpass as gp
 
-LOCAL_USER = gp.getuser()
-#WORKER = os.system("curl -s 'https://issues.feedzai.com/rest/api/2/user?username='LOCAL_USER -H 'content-type: application/json'")
+local_user = gp.getuser()
+url= 'https://issues.feedzai.com/rest/api/2/user?username=' + local_user
+header = {'Content-Type':'application/json','Accept':'application/json'}
+payload = {'username' : local_user}
 
-test = "this is" + LOCAL_USER
+def fetch_pass():
+    return open(r"/home/" + local_user + "/fz/info/jira_pw.txt").readlines()[0]
 
-print(test)
+
+#def fetch_work():
+#    return os.system('curl -s https://issues.feedzai.com/rest/api/2/user?username=' + local_user + ' -H content-type: application/json -u ' + local_user + ':' + pass_user + ' > /dev/null 2>&1')
+
+print(local_user, fetch_pass())
+r = requests.get(url, headers={"content-type":"json"}, auth=(local_user, fetch_pass))
+
+print(r.url)
+print(r.status_code)
