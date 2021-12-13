fb49a8d39062b9ee5f57353789866653e3272af7
João Pereira <joao.pereira@feedzai.com>
2021-12-13 11:58:48 +0000

    TEMPOLOG: Fixed authorization issues, overall clean-up (ditched pw encryption process for now)

--- scripts/tempolog/tempolog.py
+++ scripts/tempolog/tempolog.py
@@ -1,30 +1,16 @@
-#imports
+# Imports
 import requests
 import getpass as gp
-from cryptography.fernet import Fernet
-from requests.auth import HTTPBasicAuth
 
-
-local_user = gp.getuser()
+# Default Variables
+local_user = str(gp.getuser())
 url_user = 'https://issues.feedzai.com/rest/api/2/user?username=' + local_user
 pw_loc = '/fz/info/jira_pw.txt'
+pw = open(r"/home/" + local_user + pw_loc).readlines()[0].rstrip('\n')
 
+r = requests.get(url_user, headers={'content-type':'application/json'}, auth=(local_user, pw))
 
-key = Fernet.generate_key()
-fernet = Fernet(key)
-
-def fetch_pass():
-    global pw
-    pw = fernet.encrypt(open(r"/home/" + local_user + pw_loc).readlines()[0].encode())
-
-fetch_pass()
-
-#session = requests.Session()
-#session.auth = (local_user, pw)
-#auth = session.post(url_user)
+print(r.status_code)
 
 
 
-r = requests.get(url_user, headers={'content-type: application/json'}, auth=(local_user, fernet.decrypt(pw).decode()))
-
-#curl -s https://issues.feedzai.com/rest/api/2/user?username=joao.pereira -H content-type: application/json -u joao.pereira:password 
