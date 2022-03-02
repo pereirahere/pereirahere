ac6058157a609aeb4786e7a6bc65680ddad3fc62
João Pereira <joao.pereira@feedzai.com>
2022-03-02 23:18:51 +0000

    RUST: Playing around with borrows and references

--- udemy/courses/ultimate_rust_crash_course/ultimate_rust_crash_course-main/exercise/playground/src/main.rs
+++ udemy/courses/ultimate_rust_crash_course/ultimate_rust_crash_course-main/exercise/playground/src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
 
-    let s1 = String::from("test");
+    let mut s1 = String::from("test");
     let s2 = s1.clone();
 
-    println!("{}, {}", s1, s2)
+    println!("{}, {}", s1, s2);
+
+    do_stuff(&mut s1);
+
+    fn do_stuff(s: &mut String) {
+        s.insert_str(0, "Hi, ");
+        *s = String::from("Replace")
+    }
+
+    println!("{}", s1)
+
 }
